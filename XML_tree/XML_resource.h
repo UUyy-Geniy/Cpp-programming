e9039f03684b4d487e879b5b4a111d2290514aa4
UUyy-Geniy <jekapeheka@gmail.com>
2023-11-04 23:37:00 +0300

    add XML_resource

---
+++ XML_tree/XML_resource.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "XML_document.h"
+
+class XML_resource
+{
+private:
+	std::unique_ptr<XML_document> document;
+
+};
+
--- XML_tree/main.cpp
+++ XML_tree/main.cpp
@@ -8,4 +8,4 @@ int main() {
     doc->print();
 
     return 0;
-}
+};
